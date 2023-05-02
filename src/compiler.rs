958dd3920154bf067059aec7d7a6c2abf7452307
TheTerrior <werbird10@gmail.com>
2023-05-02 01:12:50 -0700

    Began writing tokenizer

--- src/compiler.rs
+++ src/compiler.rs
@@ -1,8 +1,34 @@
+use std::{io::{BufReader, BufRead, Write, BufWriter}, fs::{File, self}, collections::HashMap};
+
 use crate::error::QuetzalError;
 
 
-/// Compiles Quetzal assembly into Quetzal bytecode
-pub fn compile(input: &str, output: &str) -> Result<(), QuetzalError>{
+///// Compiles Quetzal assembly into Quetzal bytecode
+//pub fn compile(input: &str, output: &str) -> Result<(), QuetzalError> {
+//    Ok(())
+//}
+
+
+///// Performs lexical analysis of the input data
+/// Compiles Quetzal assembly into Quetzal bytecode line by line
+pub fn compile(input: &str, output: &str) -> Result<(), QuetzalError> {
+    let assembly_file = File::open(input).map_err(|_| QuetzalError::InputFileNotFound)?;
+    let bytecode_file = fs::File::create(output).map_err(|_| QuetzalError::InputFileNotFound)?;
+    let reader = BufReader::new(assembly_file); //don't move entire file into memory
+    let writer = BufWriter::new(bytecode_file); //reduce number of writes to storage
+    let mut variable_counter = 0;
+    let mut variable_names: HashMap<String, usize> = HashMap::new();
+
+    for line in reader.lines() {
+        let tokens: Vec<&str> = line.map_err(|_| QuetzalError::BadFileRead(String::from(input)))?.split(' ').collect(); //split line into tokens
+        
+    }
     Ok(())
+}
+
+
+#[inline]
+pub fn a() {
+
 }
 
--- src/error.rs
+++ src/error.rs
@@ -6,6 +6,9 @@ pub enum QuetzalError {
     EmptyStack, 
     ExcessiveFrameSize, 
     ExcessiveStackSize, 
+    InputFileNotFound,
+    OutputFileNotCreated,
+    BadFileRead(String),
 }
 impl std::error::Error for QuetzalError {}
 impl fmt::Display for QuetzalError {
@@ -14,6 +17,9 @@ impl fmt::Display for QuetzalError {
             Self::EmptyStack => write!(f, "Tried to pop a stack frame but the stack was empty."),
             Self::ExcessiveFrameSize => write!(f, "Tried to define a variable but the frame was too large."),
             Self::ExcessiveStackSize => write!(f, "Tried to push a new frame onto the stack but the stack was too large, stack overflow."),
+            Self::InputFileNotFound => write!(f, "Couldn't find the provided file."),
+            Self::OutputFileNotCreated => write!(f, "Couldn't create the specified file."),
+            Self::BadFileRead(file) => write!(f, "Ran into a problem reading the file '{}'", file),
         }
     }
 }
